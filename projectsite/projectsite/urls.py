3cfa7580a7d0303b0bdea5720984a6b60d91fdd9
xyrilplayz <152459199+xyrilplayz@users.noreply.github.com>
2025-03-27 14:20:46 +0800

    number 2 deplyoment pythonanwhereand 3 django readybootstrap

--- projectsite/projectsite/urls.py
+++ projectsite/projectsite/urls.py
@@ -16,7 +16,12 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from studentorg.views import HomePageView
+from studentorg import views
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    path("admin/", admin.site.urls),
+    path('', views.HomePageView.as_view(), name= 'home'),
 ]
+
+
