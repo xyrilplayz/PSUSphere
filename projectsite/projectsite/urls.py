93f011e6b61ec7794e00fd543ac14ea159e75a0a
Relova-John <170879264+Relova-John@users.noreply.github.com>
2025-03-30 15:57:53 +0800

    added search org_member, student, college, program; logout, and login

--- projectsite/projectsite/urls.py
+++ projectsite/projectsite/urls.py
@@ -15,7 +15,7 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, re_path
 from studentorg.views import (HomePageView, 
 OrganizationList, OrganizationCreateView, OrganizationUpdateView, OrganizationDeleteView, 
 OrgMemberListView, OrgMemberCreateView, OrgMemberUpdateView, OrgMemberDeleteView,
@@ -23,6 +23,7 @@ StudentListView, StudentCreateView, StudentUpdateView, StudentDeleteView,
 CollegeListView, CollegeCreateView, CollegeUpdateView, CollegeDeleteView,
 ProgramListView, ProgramCreateView, ProgramUpdateView, ProgramDeleteView)
 from studentorg import views
+from django.contrib.auth import views as auth_views
 
 urlpatterns = [
     path("admin/", admin.site.urls),
@@ -51,4 +52,7 @@ urlpatterns = [
     path('program_list/add', ProgramCreateView.as_view(), name='program-add'),
     path('program_list/<pk>', ProgramUpdateView.as_view(), name='program-update'),
     path('program_list/<pk>/delete', ProgramDeleteView.as_view(), name='program-delete'),
+
+    re_path(r'^login/$', auth_views.LoginView.as_view(template_name='login.html'), name='login'),
+    re_path(r'^logout/$', auth_views.LogoutView.as_view(template_name='logout.html'), name='logout'),
 ]
--- projectsite/studentorg/views.py
+++ projectsite/studentorg/views.py
@@ -4,15 +4,20 @@ from django.views.generic.edit import CreateView, UpdateView, DeleteView
 from studentorg.models import Organization, OrgMember, Student, College, Program
 from studentorg.forms import OrganizationForm,  OrgMemberForm, StudentForm, CollegeForm, ProgramForm
 from django.urls import reverse_lazy
+from django.utils.decorators import method_decorator
+from django.contrib.auth.decorators import login_required
 from typing import Any
 from django.db.models.query import QuerySet
 from django.db.models import Q
-
+import calendar
+import re
+@method_decorator(login_required, name='dispatch')
 class HomePageView(ListView):
     model = Organization
     context_object_name = 'home'
     template_name = "home.html"
 
+@method_decorator(login_required, name='dispatch')
 class OrganizationList(ListView):
     model = Organization
     context_object_name = 'organization'
@@ -29,110 +34,188 @@ class OrganizationList(ListView):
             )
         return qs
 
+@method_decorator(login_required, name='dispatch')
 class OrganizationCreateView(CreateView):
     model = Organization
     form_class = OrganizationForm
     template_name = 'org_add.html'
     success_url = reverse_lazy('organization-list')
     
+@method_decorator(login_required, name='dispatch')
 class OrganizationUpdateView(UpdateView):
     model = Organization
     form_class = OrganizationForm
     template_name = 'org_edit.html'
     success_url = reverse_lazy('organization-list')
 
+@method_decorator(login_required, name='dispatch')
 class OrganizationDeleteView(DeleteView):
     model = Organization
     template_name = 'org_del.html'
     success_url = reverse_lazy('organization-list')
 
+@method_decorator(login_required, name='dispatch')
 class OrgMemberListView(ListView):
     model = OrgMember
     context_object_name = 'org_member'
     template_name = "org_member_list.html"
     paginate_by = 5
 
+    def get_queryset(self, *args, **kwargs):
+        qs = super().get_queryset(*args, **kwargs)
+        query = self.request.GET.get('q')
+
+        if query:
+            filters = (
+                Q(organization__name__icontains=query) | 
+                Q(student__firstname__icontains=query) | 
+                Q(student__lastname__icontains=query) | 
+                Q(student__middlename__icontains=query)
+            )
+
+            if query.isdigit() and len(query) == 4:  
+                filters |= Q(date_joined__year=int(query))
+
+            elif query.capitalize() in calendar.month_name[1:]:  
+                month_number = list(calendar.month_name).index(query.capitalize())
+                filters |= Q(date_joined__month=month_number)
+
+            else:  
+                match = re.match(r'(\w+) (\d{4})', query)  
+                if match:
+                    month_str, year_str = match.groups()
+                    if month_str.capitalize() in calendar.month_name[1:]:
+                        month_number = list(calendar.month_name).index(month_str.capitalize())
+                        filters |= Q(date_joined__month=month_number, date_joined__year=int(year_str))
+
+            qs = qs.filter(filters)
+        return qs
+@method_decorator(login_required, name='dispatch')
 class OrgMemberCreateView(CreateView):
     model = OrgMember
     form_class = OrgMemberForm
     template_name = 'org_member_add.html'
     success_url = reverse_lazy('org-member-list')
 
+@method_decorator(login_required, name='dispatch')
 class OrgMemberUpdateView(UpdateView):
     model = OrgMember
     form_class = OrgMemberForm
     template_name = 'org_member_edit.html'
     success_url = reverse_lazy('org-member-list')
 
+@method_decorator(login_required, name='dispatch')
 class OrgMemberDeleteView(DeleteView):
     model = OrgMember
     template_name = 'org_member_del.html'
     success_url = reverse_lazy('org-member-list')
 
+@method_decorator(login_required, name='dispatch')
 class StudentListView(ListView):
     model = Student
     context_object_name = 'student'
     template_name = "student_list.html"
     paginate_by = 5
 
+    def get_queryset(self, *args, **kwargs):
+        qs = super(StudentListView, self).get_queryset(*args, **kwargs)
+        if self.request.GET.get('q'):
+            query = self.request.GET.get('q')
+            qs = qs.filter(
+                Q(student_id__icontains=query) | 
+                Q(firstname__icontains=query) | 
+                Q(lastname__icontains=query) | 
+                Q(middlename__icontains=query) |
+                Q(program__prog_name__icontains=query)
+            )
+        return qs
+
+@method_decorator(login_required, name='dispatch')
 class StudentCreateView(CreateView):
     model = Student
     form_class = StudentForm
     template_name = 'student_add.html'
     success_url = reverse_lazy('student-list')
 
+@method_decorator(login_required, name='dispatch')
 class StudentUpdateView(UpdateView):
     model = Student
     form_class = StudentForm
     template_name = 'student_edit.html'
     success_url = reverse_lazy('student-list')
 
+@method_decorator(login_required, name='dispatch')
 class StudentDeleteView(DeleteView):
     model = Student
     template_name = 'student_del.html'
     success_url = reverse_lazy('student-list')
 
+@method_decorator(login_required, name='dispatch')
 class CollegeListView(ListView):
     model = College
     context_object_name = 'college'
     template_name = "college_list.html"
     paginate_by = 5
 
+    def get_queryset(self, *args, **kwargs):
+        qs = super(CollegeListView, self).get_queryset(*args, **kwargs)
+        if self.request.GET.get('q'):
+            query = self.request.GET.get('q')
+            qs = qs.filter(
+                Q(college_name__icontains=query)
+            )
+        return qs
+@method_decorator(login_required, name='dispatch')
 class CollegeCreateView(CreateView):
     model = College
     form_class = CollegeForm
     template_name = 'college_add.html'
     success_url = reverse_lazy('college-list')
 
+@method_decorator(login_required, name='dispatch')
 class CollegeUpdateView(UpdateView):
     model = College
     form_class = CollegeForm
     template_name = 'college_edit.html'
     success_url = reverse_lazy('college-list')
 
+@method_decorator(login_required, name='dispatch')
 class CollegeDeleteView(DeleteView):
     model = College
     template_name = 'college_del.html'
     success_url = reverse_lazy('college-list')
 
+@method_decorator(login_required, name='dispatch')
 class ProgramListView(ListView):
     model = Program
     context_object_name = 'program'
     template_name = "program_list.html"
     paginate_by = 5
 
+    def get_queryset(self, *args, **kwargs):
+        qs = super(ProgramListView, self).get_queryset(*args, **kwargs)
+        if self.request.GET.get('q'):
+            query = self.request.GET.get('q')
+            qs = qs.filter(
+                Q(prog_name__icontains=query) |
+                Q(college__college_name__icontains=query)
+            )
+        return qs
+@method_decorator(login_required, name='dispatch')
 class ProgramCreateView(CreateView):
     model = Program
     form_class = ProgramForm
     template_name = 'program_add.html'
     success_url = reverse_lazy('program-list')
 
+@method_decorator(login_required, name='dispatch')
 class ProgramUpdateView(UpdateView):
     model = Program
     form_class = ProgramForm
     template_name = 'program_edit.html'
     success_url = reverse_lazy('program-list')
 
+@method_decorator(login_required, name='dispatch')
 class ProgramDeleteView(DeleteView):
     model = Program
     template_name = 'program_del.html'
