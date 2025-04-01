2a0db3281b06b2358bf08182a0563d2e62607d80
xyrilplayz <152459199+xyrilplayz@users.noreply.github.com>
2025-04-01 23:18:12 +0800

    modified version_ dropdown working

--- projectsite/projectsite/urls.py
+++ projectsite/projectsite/urls.py
@@ -54,5 +54,6 @@ urlpatterns = [
     path('program_list/<pk>/delete', ProgramDeleteView.as_view(), name='program-delete'),
 
     re_path(r'^login/$', auth_views.LoginView.as_view(template_name='login.html'), name='login'),
-    re_path(r'^logout/$', auth_views.LogoutView.as_view(template_name='logout.html'), name='logout'),
+    re_path(r'^logout/$', auth_views.LogoutView.as_view(), name='logout'),
+   # re_path(r'^login/$', auth_views.LogoutView.as_view(), name='logout'),
 ]
